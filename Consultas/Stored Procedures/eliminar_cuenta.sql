b7d058c533f1b005d59253796694cbb137aeab16
Ale Ponzo <aleponzo1@gmail.com>
2015-06-10 19:26:20 -0300

    Se termina de hacer el eliminar cuenta

--- Consultas/Stored Procedures/eliminar_cuenta.sql
+++ Consultas/Stored Procedures/eliminar_cuenta.sql
@@ -2,5 +2,11 @@ CREATE procedure dbo.eliminar_Cuenta
 	@Id_Cuenta numeric (18,0)
 AS
 	delete from dbo.Depositos WHERE Deposito_Cuenta = @Id_Cuenta
+	delete from dbo.Transferencia 
+		where Transferencia_Cuenta_Destino = @Id_Cuenta OR Transferencia_Cuenta_Emisora = @Id_Cuenta
+	delete from dbo.Retiros
+		where Retiro_Cuenta = @Id_Cuenta
 	delete from dbo.Cuentas WHERE Cuenta_Nro = @Id_Cuenta	
 GO
+
+exec eliminar_Cuenta 1111111111111115
