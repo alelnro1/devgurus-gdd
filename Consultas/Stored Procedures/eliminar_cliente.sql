62e18017f6148952d33faca911ce253e2ca2a55a
Ale Ponzo <aleponzo1@gmail.com>
2015-06-10 19:50:50 -0300

    Creado el procedure que elimina cliente

--- Consultas/Stored Procedures/eliminar_cliente.sql
+++ Consultas/Stored Procedures/eliminar_cliente.sql
@@ -1,10 +1,27 @@
 CREATE procedure dbo.eliminar_Cliente
-	@Id_Cliente numeric (18,0)
+	@Id_Cliente int
 AS
 	delete from dbo.Transaccion_Pendiente WHERE Transaccion_Pendiente_Cliente = @Id_Cliente
+	delete from Items where Items_Factura in (select factura_numero from Facturas where Factura_Cliente = @Id_Cliente)
 	delete from dbo.Facturas WHERE Factura_Cliente = @Id_Cliente
-	delete from dbo.Cuentas WHERE Cuenta_Cliente = @Id_Cliente	
+	
+	-- DE ACA SE ELIMINA CADA CUENTA DEL CLIENTE
+	DECLARE @param numeric(18,0)
+
+	DECLARE curs CURSOR LOCAL FAST_FORWARD FOR
+		select cuenta_nro from cuentas where Cuenta_Cliente = @Id_Cliente
+
+	OPEN curs
+
+	FETCH NEXT FROM curs INTO @param
+
+	WHILE @@FETCH_STATUS = 0 BEGIN
+		EXEC eliminar_Cuenta  @param
+		FETCH NEXT FROM curs INTO @param
+	END
+
+	CLOSE curs
+	DEALLOCATE curs
+	
 	delete from dbo.Clientes WHERE Cliente_Id = @Id_Cliente	
 GO
-
-//para mi quengo que hacer el exect eliminar cuentas de cliente o algo asi 
