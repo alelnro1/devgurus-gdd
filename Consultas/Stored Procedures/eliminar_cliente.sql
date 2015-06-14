f31859ff734829b6714ff59df3878ed81784f1ca
Julian <julycm92@gmail.com>
2015-06-14 14:14:07 -0300

    cambio el eliminar cliente

--- Consultas/Stored Procedures/eliminar_cliente.sql
+++ Consultas/Stored Procedures/eliminar_cliente.sql
@@ -1,27 +1,6 @@
-CREATE procedure dbo.eliminar_Cliente
+create procedure dbo.eliminar_Cliente
 	@Id_Cliente int
 AS
-	delete from dbo.Transaccion_Pendiente WHERE Transaccion_Pendiente_Cliente = @Id_Cliente
-	delete from Items where Items_Factura in (select factura_numero from Facturas where Factura_Cliente = @Id_Cliente)
-	delete from dbo.Facturas WHERE Factura_Cliente = @Id_Cliente
-	
-	-- DE ACA SE ELIMINA CADA CUENTA DEL CLIENTE
-	DECLARE @param numeric(18,0)
-
-	DECLARE curs CURSOR LOCAL FAST_FORWARD FOR
-		select cuenta_nro from cuentas where Cuenta_Cliente = @Id_Cliente
-
-	OPEN curs
-
-	FETCH NEXT FROM curs INTO @param
-
-	WHILE @@FETCH_STATUS = 0 BEGIN
-		EXEC eliminar_Cuenta  @param
-		FETCH NEXT FROM curs INTO @param
-	END
-
-	CLOSE curs
-	DEALLOCATE curs
-	
-	delete from dbo.Clientes WHERE Cliente_Id = @Id_Cliente	
+ 
+UPDATE Clientes SET Cliente_Estado = 'Inhabilitado' where Cliente_Id = @Id_Cliente
 GO
