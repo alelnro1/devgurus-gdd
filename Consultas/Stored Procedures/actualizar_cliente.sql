7e9113144dbdb8df2a4d5320448102430da6b5cb
Julian <julycm92@gmail.com>
2015-06-16 19:50:51 -0300

    actualizar cliente terminado sql sprocedimiento

--- Consultas/Stored Procedures/actualizar_cliente.sql
+++ Consultas/Stored Procedures/actualizar_cliente.sql
@@ -1,25 +1,31 @@
 CREATE PROCEDURE actualizar_cliente
-	@id_cliente int,@nombre varchar(255),@estado varchar(255),@apellido varchar(255), @tipo_doc int, @nro_doc int,@nacionalidad varchar(255),@pais int, @localidad varchar(255),
-	@Dom_Calle varchar(255), @Dom_Nro numeric(18,0),@Dom_Piso numeric(18,0), @Dom_Depto varchar(10),@Fecha_Nac datetime,@Mail varchar(255),@User int
+	@id_cliente int,@nombre varchar(255),@estado varchar(255),@apellido varchar(255),
+	 @tipo_doc varchar(255), @nro_doc int,@nacionalidad varchar(255),@pais varchar(255), @localidad varchar(255),
+	@Dom_Calle varchar(255), @Dom_Nro numeric(18,0),@Dom_Piso numeric(18,0), @Dom_Depto varchar(10),@Mail varchar(255)
 
 AS
-	
+	declare @tipo_documento_id int
+	select @tipo_documento_id = Tipo_Doc_Id from Tipo_De_Doc where @tipo_doc = Tipo_Doc_Desc;
+	declare @pais_id int
+	select  @pais_id = Pais_Id from Paises where Pais_Nombre=@pais;
+
 	UPDATE [GD1C2015].[dbo].[Clientes]
    SET [Cliente_Nombre] = @nombre
 	  ,[Cliente_Estado]= @estado
       ,[Cliente_Apellido] = @apellido
-      ,[Cliente_Tipo_Doc] = @tipo_doc
+      ,[Cliente_Tipo_Doc] = @tipo_documento_id
       ,[Cliente_Nro_Doc] =  @nro_doc 
       ,[Cliente_Nacionalidad] = @nacionalidad
-      ,[Cliente_Pais] = @pais 
+      ,[Cliente_Pais] = @pais_id 
       ,[Cliente_Localidad] = @localidad
       ,[Cliente_Dom_Calle] = @Dom_Calle
       ,[Cliente_Dom_Nro] = @Dom_Nro
       ,[Cliente_Dom_Piso] = @Dom_Piso
       ,[Cliente_Dom_Depto] = @Dom_Depto
-      ,[Cliente_Fecha_Nac] = @Fecha_Nac
       ,[Cliente_Mail] = @Mail
-      ,[Cliente_User] = @User
+
  WHERE @id_cliente = Cliente_Id
+ 
 
 GO
+
