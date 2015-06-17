efddf436b034cb9c92aa8d969ebe763df55bf402
Julian <julycm92@gmail.com>
2015-06-17 04:48:06 -0300

    actualizo a cerrado

--- Consultas/Stored Procedures/eliminar_cuenta.sql
+++ Consultas/Stored Procedures/eliminar_cuenta.sql
@@ -1,11 +1,12 @@
-CREATE procedure dbo.eliminar_Cuenta
+Create procedure dbo.eliminar_Cuenta
 	@Id_Cuenta numeric (18,0)
 AS
-	delete from dbo.Depositos WHERE Deposito_Cuenta = @Id_Cuenta
-	delete from dbo.Transferencia 
-		where Transferencia_Cuenta_Destino = @Id_Cuenta OR Transferencia_Cuenta_Emisora = @Id_Cuenta
-	delete from dbo.Retiros
-		where Retiro_Cuenta = @Id_Cuenta
-	delete from dbo.Cuentas WHERE Cuenta_Nro = @Id_Cuenta	
+
+	UPDATE [GD1C2015].[dbo].[Cuentas]
+   
+   SET [Cuenta_Estado] = 'Cerrada'
+
+ WHERE Cuenta_Nro = @Id_Cuenta;
+
 GO
 
