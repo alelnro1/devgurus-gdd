ead9996970f1a836a8b7366285edbf81f9da2803
Julian <julycm92@gmail.com>
2015-06-28 05:17:40 -0300

    se cambio este ojo q no andaba --- arreglado

--- Consultas/Stored Procedures/actualizar_cliente.sql
+++ Consultas/Stored Procedures/actualizar_cliente.sql
@@ -1,14 +1,15 @@
 CREATE PROCEDURE actualizar_cliente
 	@id_cliente int,@nombre varchar(255),@estado varchar(255),@apellido varchar(255),
 	 @tipo_doc varchar(255), @nro_doc int,@nacionalidad varchar(255),@pais varchar(255), @localidad varchar(255),
-	@Dom_Calle varchar(255), @Dom_Nro numeric(18,0),@Dom_Piso numeric(18,0), @Dom_Depto varchar(10),@Mail varchar(255)
+	@Dom_Calle varchar(255), @Dom_Nro numeric(18,0),@Dom_Piso numeric(18,0), @Dom_Depto varchar(10),@fecha_nacimiento_recibida varchar(255), @Mail varchar(255)
 
 AS
 	declare @tipo_documento_id int
 	select @tipo_documento_id = Tipo_Doc_Id from Tipo_De_Doc where @tipo_doc = Tipo_Doc_Desc;
 	declare @pais_id int
 	select  @pais_id = Pais_Id from Paises where Pais_Nombre=@pais;
-
+	declare @fecha_nacimiento_a_insertar datetime
+	SELECT  @fecha_nacimiento_a_insertar = CAST(@fecha_nacimiento_recibida AS datetime);
 	UPDATE [GD1C2015].[dbo].[Clientes]
    SET [Cliente_Nombre] = @nombre
 	  ,[Cliente_Estado]= @estado
@@ -22,10 +23,10 @@ AS
       ,[Cliente_Dom_Nro] = @Dom_Nro
       ,[Cliente_Dom_Piso] = @Dom_Piso
       ,[Cliente_Dom_Depto] = @Dom_Depto
+      ,[Cliente_Fecha_Nac]= @fecha_nacimiento_a_insertar
       ,[Cliente_Mail] = @Mail
+      
 
  WHERE @id_cliente = Cliente_Id
  
-
 GO
-
