002d3922af5bce398966173abd4b047735110622
maxmouser <maxibm@yahoo.com.ar>
2015-06-30 15:20:16 -0300

    se cambio cerradA por cerradO eliminarCuenta

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -574,7 +574,7 @@ GO
 CREATE PROCEDURE DEVGURUS.eliminar_Cuenta
 	@Id_Cuenta numeric (18,0)
 AS
-	UPDATE DEVGURUS.Cuentas SET Cuenta_Estado = 'Cerrada' WHERE Cuenta_Nro = @Id_Cuenta;
+	UPDATE DEVGURUS.Cuentas SET Cuenta_Estado = 'Cerrado' WHERE Cuenta_Nro = @Id_Cuenta;
 GO
 	Print 'El procedimiento ELIMINAR CUENTA se ha creado correctamente';
 
