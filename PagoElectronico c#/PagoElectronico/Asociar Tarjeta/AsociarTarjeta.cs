6a5debc343029f598a363f038d422235b93d495c
Julian <julycm92@gmail.com>
2015-07-01 15:40:01 -0300

    ahora anda el asociar tarjetas

--- PagoElectronico c#/PagoElectronico/Asociar Tarjeta/AsociarTarjeta.cs
+++ PagoElectronico c#/PagoElectronico/Asociar Tarjeta/AsociarTarjeta.cs
@@ -36,7 +36,7 @@ namespace PagoElectronico
 
                 SqlDataReader lector = tarjetaDAO.buscarTarjetas(cliente_id_para_tarjetas.getCliente_Id());
                 List<DataGridViewRow> filas = new List<DataGridViewRow>();
-                Object[] columnas = new Object[5];
+                Object[] columnas = new Object[6];
                 while (lector.Read()) {
 
                     columnas[0] = lector["Tarjeta_Id"];
@@ -44,7 +44,7 @@ namespace PagoElectronico
                     columnas[2] = lector["Tarjeta_Emisor_Desc"];
                     columnas[3] = lector["Tarjeta_Fecha_Emision"]; 
                     columnas[4] = lector["Tarjeta_Fecha_Vencimiento"];
-        
+                    columnas[5] = lector["Tarjeta_Asociada"];
 
                     filas.Add(new DataGridViewRow());
                     filas[filas.Count - 1].CreateCells(lista_Tarjetas, columnas);
@@ -63,5 +63,34 @@ namespace PagoElectronico
         {
 
         }
+
+        private void cambiar_asociamiento_tarjeta(object sender, EventArgs e,string valor_asociacion)
+        {
+             try { DataGridViewRow fila = lista_Tarjetas.SelectedRows[0]; 
+            
+
+                String id_tarjeta;
+                id_tarjeta = (fila.Cells[0].Value.ToString());
+                string estado = (fila.Cells[5].Value.ToString());
+                 tarjetaDAO.update_asociada_tarjeta(id_tarjeta,valor_asociacion);
+                 this.ejecutarBusquedaTarjetas();
+                 MessageBox.Show("Se asocio correctamente la tarjeta N° "+ id_tarjeta + " al Cliente id N° "+ cliente_id_para_tarjetas.getCliente_Id()  , "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+             }
+            
+            
+            catch { MessageBox.Show("Debe seleccionar el Usuario antes de aceptar", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning); 
+            }
+        }
+        private void buttom_Asociar_Click(object sender, EventArgs e)
+        {
+            cambiar_asociamiento_tarjeta(sender, e, "Asociada");
+            
+        }
+
+        private void boton_Desasociar_Click(object sender, EventArgs e)
+        {
+            cambiar_asociamiento_tarjeta(sender, e, "Desasociada");
+        }
+        
     }
 }
--- PagoElectronico c#/PagoElectronico/Base de datos/Conexion/TarjetaDAO.cs
+++ PagoElectronico c#/PagoElectronico/Base de datos/Conexion/TarjetaDAO.cs
@@ -21,12 +21,21 @@ namespace PagoElectronico.BaseDeDatos.Conexion
         {
 
 
-            String sentenciaSQL = "select Tarjeta_Id, Tarjeta_Digitos_Visibles,Tarjeta_Emisor_Desc,Tarjeta_Fecha_Emision,Tarjeta_Fecha_Vencimiento FROM " + ConstantesBD.t_tarjetas + " where Tarjeta_Cliente ='" + cliente_id + "'";
+            String sentenciaSQL = "select Tarjeta_Id, Tarjeta_Digitos_Visibles,Tarjeta_Emisor_Desc,Tarjeta_Fecha_Emision,Tarjeta_Fecha_Vencimiento,Tarjeta_Asociada FROM " + ConstantesBD.t_tarjetas + " where Tarjeta_Cliente ='" + cliente_id + "'";
 
            
             return this.GD1C2015.ejecutarSentenciaConRetorno(sentenciaSQL);
         }
 
 
+
+        public void update_asociada_tarjeta(string id_tarjeta, string estado)
+        {
+            string sql;
+            sql = "UPDATE " + ConstantesBD.t_tarjetas + " SET Tarjeta_Asociada = '" + estado + "' WHERE Tarjeta_Id = " + id_tarjeta;
+
+
+            this.GD1C2015.ejecutarSentenciaSinRetorno(sql);
+        }
     }
 }
