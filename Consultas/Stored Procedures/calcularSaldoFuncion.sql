94c511896f13e408824c9602401f16584054fc90
maxmouser <maxibm@yahoo.com.ar>
2015-07-06 15:31:17 -0300

    corregida calcularsaldo

--- Consultas/Stored Procedures/calcularSaldoFuncion.sql
+++ Consultas/Stored Procedures/calcularSaldoFuncion.sql
@@ -1,10 +1,9 @@
-create function funcionCalcularSaldo (@cuenta_nro numeric(18,0))
+create function DEVGURUS.funcionCalcularSaldo (@cuenta_nro numeric(18,0))
 
 RETURNS float
- WITH EXECUTE AS CALLER
+WITH EXECUTE AS CALLER
 AS
 BEGIN
-
 
 declare @importe_total_depositos float
 select @importe_total_depositos = sum( Deposito_Importe)from gd_esquema.Maestra  where Cuenta_Numero = @cuenta_nro
@@ -24,7 +23,10 @@ declare @saldo float
 select @saldo =  @importe_total_depositos + @importe_total_transferencias_a_la_cuenta  + @importe_total_retiros - @importe_total_transferencias_desde_la_cuenta
 
 
-UPDATE DEVGURUS.Cuentas SET Cuenta_Saldo = @saldo where Cuenta_Nro = @cuenta_nro
+RETURN(@saldo)
 
-END
+END;
+
+GO
+
 
